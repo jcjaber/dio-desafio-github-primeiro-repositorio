aec4bd2708dbd7e8b7d7466d0abb04495cf80693
Cassio Jaber <jcjaber@gmail.com>
2022-02-11 10:11:45 -0300

    CRUD de Usuário feito

---
+++ 29. Explorando o Estilo Arquitetural REST com NodeJS/node-user-authentication-api/src/routes/users.route.ts
@@ -0,0 +1,45 @@
+import { Router, Request, Response, NextFunction } from "express";
+import { StatusCodes } from 'http-status-codes';
+
+// get /users 
+
+const usersRoute = Router();
+
+usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
+  const users = [ {userName: 'Renan'}];
+  res.status(StatusCodes.OK).send(users);
+});
+
+// get /users/:uuid 
+
+usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+  const uuid = req.params.uuid;
+  res.status(200).send( { uuid });
+});
+
+// post /users
+
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+  const newUser = req.body;
+  res.status(StatusCodes.CREATED).send(newUser)
+});
+
+// put /users/:uuid 
+
+usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+  const uuid = req.params.uuid;
+  const modifiedUser = req.body;
+
+  modifiedUser.uuid = uuid;
+
+  res.status(StatusCodes.OK).send(modifiedUser);
+})
+
+// delete /users/:uuid 
+
+usersRoute.delete('.users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+  const uuid = req.params.uuid;
+  res.sendStatus(StatusCodes.OK)
+})
+
+export default usersRoute;
